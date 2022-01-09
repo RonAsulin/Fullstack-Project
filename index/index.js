5deb30b596fe9b6c913d95541a93531d3601bbf1
ronasolin12 <ronasolin12@gmail.com>
2022-01-09 15:19:58 +0200

    09012022_1

--- index/index.js
+++ index/index.js
@@ -2,8 +2,8 @@ function check()
 {
     let name = document.getElementById("user").value;
     let pass = document.getElementById("password").value;
-    let users = ["Ron","Yulia,Igor"];
-    let passwords = ["12345","12345,12345"];
+    let users = ["Ron","Yulia","Igor"];
+    let passwords = ["12345","12345","12345"];
     flag= false
     for(i=0; i<3;i++)
     if(name == users[i] && pass == passwords[i])
